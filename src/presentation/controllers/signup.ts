73cf79fa689ce9d360b1b7081ff210ca3c7ae3b4
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-04-18 12:20:27 -0300

    refactor: simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,13 +1,16 @@
+/* eslint-disable consistent-return */
 import { HttpResponse, HttpRequest } from '../protocols/http';
 import { MissingParamError } from '../errors/missing-param-error';
 import { badRequest } from '../helpers/http-helper';
 
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'));
+    const requiredFields = ['name', 'email'];
+    // eslint-disable-next-line no-restricted-syntax
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field));
+      }
     }
-
-    return badRequest(new MissingParamError('email'));
   }
 }
