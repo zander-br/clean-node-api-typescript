0b5c34c1f5c63946e6c02c81bc2eb387a892a8a6
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-04-18 11:45:40 -0300

    feat: ensure SignUpController returns 400 if no name is provider

---
+++ src/presentation/controllers/signup.ts
@@ -0,0 +1,8 @@
+export class SignUpController {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  handle(httpRequest: any): any {
+    return {
+      statusCode: 400,
+    };
+  }
+}
