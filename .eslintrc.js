5d302dc3d83bd4d2e6f2a50b9a5a7fa897c2044c
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-04-18 11:14:57 -0300

    chore: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'airbnb-typescript/base'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'class-methods-use-this': 'off',
+    '@typescript-eslint/require-await': 'off',
+    'no-underscore-dangle': 'off',
+    'import/prefer-default-export': 'off',
+    'import/export': 'off',
+    'lines-between-class-members': 'off',
+    'max-classes-per-file': 'off'
+  },
+};
