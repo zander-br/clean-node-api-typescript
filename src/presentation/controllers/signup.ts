6f82ef3afe0c5a9b30d85be95009cfec993cc519
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-04-18 11:56:30 -0300

    feat: ensure SignUpController returns 400 if no email is provider

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,16 @@
 export class SignUpController {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   handle(httpRequest: any): any {
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name'),
+      };
+    }
+
     return {
       statusCode: 400,
-      body: new Error('Missing param: name'),
+      body: new Error('Missing param: email'),
     };
   }
 }
