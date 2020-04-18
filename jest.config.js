6e0566e7013662bbb0ebb2ebe72fb65a437105ca
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-04-18 11:40:26 -0300

    chore: change jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  roots: ['<rootDir/src'],
-  collectCoverageFrom: ['<rootDir/src/**/*.ts>'],
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts>'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
