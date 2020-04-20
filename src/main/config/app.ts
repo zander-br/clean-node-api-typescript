26588e3934a348fd6e34449ef1f1af246e3ff0d3
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-04-20 17:30:52 -0300

    feat: ensure signup routes return an account on success

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import setupMiddewares from './middlewares';
+import setupRoutes from './routes';
 
 const app = express();
 
 setupMiddewares(app);
+setupRoutes(app);
 
 export default app;
