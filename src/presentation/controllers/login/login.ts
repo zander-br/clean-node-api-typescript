6170dd0cb39a96aca390d98d1174aa9b0036b88c
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-04-21 14:26:34 -0300

    refactor: remove code duplicate

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -12,19 +12,21 @@ export class LoginController implements Controller {
 
   // eslint-disable-next-line consistent-return
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    if (!httpRequest.body.email) {
+    const { email, password } = httpRequest.body;
+
+    if (!email) {
       return new Promise((resolve) => resolve(
         badRequest(new MissingParamError('email')),
       ));
     }
 
-    if (!httpRequest.body.password) {
+    if (!password) {
       return new Promise((resolve) => resolve(
         badRequest(new MissingParamError('password')),
       ));
     }
 
-    const isValid = this.emailValidator.isValid(httpRequest.body.email);
+    const isValid = this.emailValidator.isValid(email);
     if (!isValid) {
       return new Promise((resolve) => resolve(
         badRequest(new InvalidParamError('email')),
