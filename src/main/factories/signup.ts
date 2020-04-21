90fea79963ba9c51ba1cc803f6d82e030e985fb8
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-04-21 11:23:37 -0300

    feat: ensure LogControllerDecorator calls controller with correct value

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,14 +3,17 @@ import { EmailValidatorAdapter } from '../../utils/email-validator-adapter';
 import { DbAddAccount } from '../../data/usecase/add-account/db-add-account';
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter';
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account';
+import { Controller } from '../../presentation/protocols';
+import { LogControllerDecorator } from '../decorators/log';
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const SALT = 12;
 
   const emailValidatorAdapter = new EmailValidatorAdapter();
   const bcryptAdapter = new BcryptAdapter(SALT);
   const accountMongoRepository = new AccountMongoRepository();
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository);
+  const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount);
 
-  return new SignUpController(emailValidatorAdapter, dbAddAccount);
+  return new LogControllerDecorator(signUpController);
 };
