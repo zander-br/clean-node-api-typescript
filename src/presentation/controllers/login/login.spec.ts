7180e60ed542d849feca5f6b9da1d2e24a4258f8
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-04-21 14:55:20 -0300

    test: ensure LoginController returns 401 if invalid credentials are provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { LoginController } from './login';
-import { badRequest, serverError } from '../../helpers/http-helper';
+import { badRequest, serverError, unauthorized } from '../../helpers/http-helper';
 import { MissingParamError, InvalidParamError } from '../../errors';
 import { EmailValidator } from '../../protocols/email-validator';
 import { HttpRequest } from '../../protocols';
@@ -108,5 +108,14 @@ describe('Login Controller', () => {
 
     await sut.handle(makeFakeRequest());
     expect(authSpy).toHaveBeenCalledWith('any_email@mail.com', 'any_password');
+  });
+
+  test('Should return 401 if invalid credentials are provided', async () => {
+    const { sut, authenticationStub } = makeSut();
+    jest.spyOn(authenticationStub, 'auth')
+      .mockReturnValueOnce(new Promise((resolve) => resolve(null)));
+
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(unauthorized());
   });
 });
