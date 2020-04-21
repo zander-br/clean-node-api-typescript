d6fd40e44f09dbe1fff9ac9b288038fa43a8d931
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-04-21 14:43:26 -0300

    feat: ensure: LoginController calls Authentication with correct values

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -2,12 +2,15 @@ import { Controller, HttpRequest, HttpResponse } from '../../protocols';
 import { badRequest, serverError } from '../../helpers/http-helper';
 import { MissingParamError, InvalidParamError } from '../../errors';
 import { EmailValidator } from '../../protocols/email-validator';
+import { Authentication } from '../../../domain/usecases/authentication';
 
 export class LoginController implements Controller {
   private readonly emailValidator: EmailValidator;
+  private readonly authentication: Authentication;
 
-  constructor(emailValidator: EmailValidator) {
+  constructor(emailValidator: EmailValidator, authentication: Authentication) {
     this.emailValidator = emailValidator;
+    this.authentication = authentication;
   }
 
   // eslint-disable-next-line consistent-return
@@ -33,6 +36,8 @@ export class LoginController implements Controller {
           badRequest(new InvalidParamError('email')),
         ));
       }
+
+      await this.authentication.auth(email, password);
     } catch (error) {
       return serverError(error);
     }
