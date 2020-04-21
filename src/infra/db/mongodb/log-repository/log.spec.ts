c3b9c3e5ad9add8d1e9bd9880183860badd3b5f6
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-04-21 13:28:24 -0300

    refactor: move sut creation to a factory helper method

--- src/infra/db/mongodb/log-repository/log.spec.ts
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -3,6 +3,8 @@ import { Collection } from 'mongodb';
 import { MongoHelper } from '../helpers/mongo-helper';
 import { LogMongoRepository } from './log';
 
+const makeSut = (): LogMongoRepository => new LogMongoRepository();
+
 describe('Log Mongo Repository', () => {
   let errorCollection: Collection;
 
@@ -20,7 +22,7 @@ describe('Log Mongo Repository', () => {
   });
 
   test('Should create an error log on success', async () => {
-    const sut = new LogMongoRepository();
+    const sut = makeSut();
     await sut.logError('any_error');
 
     const count = await errorCollection.countDocuments();
