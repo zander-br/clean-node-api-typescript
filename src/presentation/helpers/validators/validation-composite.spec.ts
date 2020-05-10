4bdc3598404fd49741dea2137afdd5a4586a7096
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-05-10 08:07:28 -0300

    refactor: move sut creation to a factory method

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -3,17 +3,37 @@ import { ValidationComposite } from './validation-composite';
 import { MissingParamError } from '../../errors';
 import { Validation } from './validation';
 
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    validate(input: any): Error {
+      return null;
+    }
+  }
+
+  return new ValidationStub();
+};
+
+interface SutTypes {
+  sut: ValidationComposite;
+  validationStub: Validation;
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidation();
+  const sut = new ValidationComposite([validationStub]);
+
+  return {
+    sut,
+    validationStub,
+  };
+};
+
 describe('Validation Composite', () => {
   test('Should return an error if any validation fails', () => {
-    class ValidationStub implements Validation {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      validate(input: any): Error {
-        return new MissingParamError('field');
-      }
-    }
+    const { sut, validationStub } = makeSut();
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('field'));
 
-    const validationStub = new ValidationStub();
-    const sut = new ValidationComposite([validationStub]);
     const error = sut.validate({ field: 'any_value' });
 
     expect(error).toEqual(new MissingParamError('field'));
