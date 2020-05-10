ec4a234ce21766df0716421a988f381a38afd264
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-05-10 10:24:43 -0300

    feat: add LoginValidationFactory

---
+++ src/main/factories/login/login-validation.ts
@@ -0,0 +1,16 @@
+/* eslint-disable no-restricted-syntax */
+import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite';
+import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation';
+import { EmailValidation } from '../../../presentation/helpers/validators/email-validation';
+import { Validation } from '../../../presentation/helpers/validators/validation';
+import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter';
+
+export const makeLoginValidation = (): ValidationComposite => {
+  const validations: Validation[] = [];
+  for (const field of ['email', 'password']) {
+    validations.push(new RequiredFieldValidation(field));
+  }
+
+  validations.push(new EmailValidation('email', new EmailValidatorAdapter()));
+  return new ValidationComposite(validations);
+};
--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,36 +1,28 @@
 import {
-  Controller, HttpRequest, HttpResponse, EmailValidator, Authentication,
+  Controller, HttpRequest, HttpResponse, Authentication, Validation,
 } from './login-protocols';
 import {
   badRequest, serverError, unauthorized, ok,
 } from '../../helpers/http-helper';
-import { MissingParamError, InvalidParamError } from '../../errors';
 
 export class LoginController implements Controller {
-  private readonly emailValidator: EmailValidator;
+  private readonly validation: Validation;
   private readonly authentication: Authentication;
 
-  constructor(emailValidator: EmailValidator, authentication: Authentication) {
-    this.emailValidator = emailValidator;
+  constructor(authentication: Authentication, validation: Validation) {
     this.authentication = authentication;
+    this.validation = validation;
   }
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const requiredFields = ['email', 'password'];
-      // eslint-disable-next-line no-restricted-syntax
-      for (const field of requiredFields) {
-        if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field));
-        }
+      const error = this.validation.validate(httpRequest.body);
+
+      if (error) {
+        return badRequest(error);
       }
 
       const { email, password } = httpRequest.body;
-
-      const isValid = this.emailValidator.isValid(email);
-      if (!isValid) {
-        return badRequest(new InvalidParamError('email'));
-      }
 
       const accessToken = await this.authentication.auth(email, password);
       if (!accessToken) {
