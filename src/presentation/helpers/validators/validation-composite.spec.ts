ec2d12b8d1d568ebd9f005be3ca23236236a9ce1
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-05-10 08:03:03 -0300

    test: ensure ValidationComposite returns an error if any validation fails

---
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ValidationComposite } from './validation-composite';
+import { MissingParamError } from '../../errors';
+import { Validation } from './validation';
+
+describe('Validation Composite', () => {
+  test('Should return an error if any validation fails', () => {
+    class ValidationStub implements Validation {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      validate(input: any): Error {
+        return new MissingParamError('field');
+      }
+    }
+
+    const validationStub = new ValidationStub();
+    const sut = new ValidationComposite([validationStub]);
+    const error = sut.validate({ field: 'any_value' });
+
+    expect(error).toEqual(new MissingParamError('field'));
+  });
+});
