771a11b0788ace105833c83c4cb654c3c82109af
Anderson Santos <anderson.feitoza.santos@gmail.com>
2020-05-13 18:29:51 -0300

    test: login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -53,5 +53,15 @@ describe('Login Routes', () => {
         })
         .expect(200);
     });
+
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'anderson.feitoza.santos@gmail.com',
+          password: '123456',
+        })
+        .expect(401);
+    });
   });
 });
